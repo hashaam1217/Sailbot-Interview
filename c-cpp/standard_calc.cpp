4a6d7d84550ad79a0d8afa9a3e2dffcfc1411f62
Hashaam Zafar <hashaam1217@gmail.com>
2023-08-23 02:17:53 +0500

    Final touches

--- c-cpp/standard_calc.cpp
+++ c-cpp/standard_calc.cpp
@@ -23,12 +23,15 @@ float bound_to_180(float angle) {
     float angle_radians = angle * M_PI / 180;
     float bounded_angle= 2.0 * atan(tan(0.5 * angle_radians));
 
+    //Edge case as -180 is included and 180 is not
     if (bounded_angle == -180.0)
     {
         bounded_angle*= -1;
     }
 
+    //Conversion to degrees
     bounded_angle *= 180 / M_PI;
+
     bounded_angle = round(bounded_angle* 10 / 10);
 
     return bounded_angle;
@@ -50,6 +53,7 @@ bool is_angle_between(float first_angle, float middle_angle, float second_angle)
     
     float angle_difference = abs(second_angle - first_angle);
 
+    //Checks for edge case of a straight line where reflex would not exist
     if (angle_difference == 0.0 || angle_difference == 180.0 || angle_difference == 360.0)
     {
         return true;
@@ -58,6 +62,7 @@ bool is_angle_between(float first_angle, float middle_angle, float second_angle)
     bool normal_reflex = (angle_difference > 180);
     bool between_normal_angle = (second_angle > middle_angle && middle_angle > first_angle);
 
+    //Relation can be simplified using a K-map, showing that it is a XOR gate
     return normal_reflex ^ between_normal_angle;
 }
 
